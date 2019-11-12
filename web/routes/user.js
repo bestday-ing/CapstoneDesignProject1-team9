002c7485f2d71515d52a3edbfecb185c86fa778c
jjoho95 <43994591+jjoho95@users.noreply.github.com>
2019-11-12 23:31:01 +0900

    changed rendering for google trends api
    1. 테스트용으로 doho function을 넣어 테스트해보고 실행결과를 나오게함

--- web/routes/user.js
+++ web/routes/user.js
@@ -8,24 +8,24 @@ var showResults = function(req, res) {
     var keyword = req.param('keyword');
     var startDate = req.param('startDate');
     var endDate = req.param('endDate');
-    
+
     console.log('/showResults 사용자가 입력한 검색어는 ' + keyword);
-    console.log('/showResults 사용자가 입력한 날짜 ' + startDate + ' ' + endDate );
-    data_Processing( [keyword, startDate, endDate] , "searchResult.ejs", req, res);
+    console.log('/showResults 사용자가 입력한 날짜 ' + startDate + ' ' + endDate);
+    data_Processing([keyword, startDate, endDate], "searchResult.ejs", req, res);
 };
 
 
 var compareKeywords = function(req, res) {
     console.log('/compareKeywords 패스 요청됨.');
     var compare = req.param('compare');
-    
+
     console.log('/compareKeywords 사용자가 입력한 비교 검색어는 ' + compare);
-    data_Processing( [compare] , "keywordCompare.ejs", req, res );
+    data_Processing([compare], "keywordCompare.ejs", req, res);
 
 };
 
-function data_Processing( args , ejsFileName, req, res ){
-    
+function data_Processing(args, ejsFileName, req, res) {
+
     let { PythonShell } = require('python-shell')
 
     var options = {
@@ -37,46 +37,112 @@ function data_Processing( args , ejsFileName, req, res ){
     };
 
     PythonShell.run('data_processing.py', options, function(err, results) {
-    if (err) throw err;
-        
-    if(results[0]=='성공'){
-        console.log()
-        var obj = JSON.parse(results[1]); // JSON object
-        //console.log(obj)
-        //console.log(obj.naver.graphData) // 이게 그 기존의 배열
+        if (err) throw err;
 
-        var NaverPeriod = [];
-        var NaverCount = [];
+        if (results[0] == '성공') {
+            console.log()
+            var obj = JSON.parse(results[1]); // JSON object
+            //console.log(obj)
+            //console.log(obj.naver.graphData) // 이게 그 기존의 배열
 
-        for (var i = 0; i < obj.naver.graphData.length; i++) {
-            NaverPeriod.push(obj.naver.graphData[i][0]);
-            NaverCount.push(obj.naver.graphData[i][2]);
-        }
+            var NaverPeriod = [];
+            var NaverCount = [];
 
-
-        obj.naver.graphData = {
-            'period' : NaverPeriod,
-            'count' : NaverCount
-        }
-        
-        
-        string = JSON.stringify(obj);
-        obj["JSONString"] = string;
-        //obj["keyword"] = keyword;
-        console.log(obj);
-
-       var context = { keyword: args[0], dataSet : obj };
-        req.app.render(ejsFileName, context, function(err, html) {
-            if (err) {
-                console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
-                return;
+            for (var i = 0; i < obj.naver.graphData.length; i++) {
+                NaverPeriod.push(obj.naver.graphData[i][0]);
+                NaverCount.push(obj.naver.graphData[i][2]);
             }
-            res.end(html);
-        });
+
+
+            obj.naver.graphData = {
+                'period': NaverPeriod,
+                'count': NaverCount
+            }
+
+
+            string = JSON.stringify(obj);
+            obj["JSONString"] = string;
+            //obj["keyword"] = keyword;
+            console.log(obj);
+
+            var context = { keyword: args[0], dataSet: obj };
+            req.app.render(ejsFileName, context, function(err, html) {
+                if (err) {
+                    console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
+                    return;
+                }
+                res.end(html);
+            });
         }
     });
+}
+
+
+var doho = function(req, res) {
+    console.log('/doho 패스 요청됨.');
+    var reqkeyword = req.param('keyword');
+    var startDate = req.param('startDate');
+    var endDate = req.param('endDate');
+
+    console.log('/doho 사용자가 입력한 검색어는 ' + reqkeyword);
+    console.log('/doho 사용자가 입력한 날짜 ' + startDate + ' ' + endDate);
+
+
+
+    // google trends json object 받아오기
+    var googleTrends = require('../node_modules/google-trends-api/lib/google-trends-api.min.js');
+    // const fs = require('fs');
+    var dohoobj = new Object();
+    var googlePeriod = [];
+    var googleCount = [];
+    let trendobj;
+
+    googleTrends.interestOverTime({
+        keyword: reqkeyword,
+        startTime: new Date(startDate),
+        endTime: new Date(endDate),
+        granularTimeResolution: true,
+    }, function(err, results) {
+        if (err) console.log('oh no error!', err);
+        // else console.log(JSON.parse(results).value);
+        else {
+            trendobj = JSON.parse(results);
+            // for (var i = 0; i < 10; i++)
+            //     console.log(obj['default']['timelineData'][i]); 
+            for (var i = 0; i < trendobj['default']['timelineData'].length; i++) {
+                var changedDate;
+                changedDate = new Date(trendobj['default']['timelineData'][i]['formattedAxisTime']);
+                changedDate = changedDate.toISOString().substring(0,10).toString();
+                console.log(changedDate);
+                googlePeriod.push(changedDate);
+                googleCount.push(trendobj['default']['timelineData'][i]['value'][0]);
+            }
+
+
+            
+            // fs.writeFileSync('test.json', results, 'utf-8');
+            //console.log(googlePeriod);
+            //console.log(googleCount);
+            dohoobj = {
+                'period': googlePeriod,
+                'count': googleCount
+            };
+
+            var context = { keyword: reqkeyword, dataSet: dohoobj };
+            req.app.render("doho.ejs", context, function(err, html) {
+                if (err) {
+                    console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
+                    return;
+                }
+                res.end(html);
+            });
+        }
+    });
+
+
 }
 
 module.exports.loadFirstPage = loadFirstPage;
 module.exports.showResults = showResults;
 module.exports.compareKeywords = compareKeywords;
+module.exports.doho = doho;
